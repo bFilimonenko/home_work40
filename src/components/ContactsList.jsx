409aeb4a43efcb105d9c99e22c971d44259fdec0
bogdanafilimonenko <bfilimonenko@gmail.com>
2025-03-20 19:36:32 +0200

    feature: styling contacts list

--- src/components/ContactsList.jsx
+++ src/components/ContactsList.jsx
@@ -1,10 +1,34 @@
+import "./ContactsList.css";
+import { Delete, Edit } from "@mui/icons-material";
+import { IconButton } from "@mui/material";
+
 export const ContactsList = ({ contacts }) => {
   return (
     <>
-      {contacts.map(contact => (<div key={contact.id}>
-        <p>{contact.name}</p>
-        <p>{contact.phone}</p>
-      </div>))}
+      <div className="contacts-table">
+        <div className="header-container">
+          <p className="first-name">FIRST NAME</p>
+          <p className="last-name">LAST NAME</p>
+          <p className="phone">PHONE</p>
+          <p className="actions">ACTIONS</p>
+        </div>
+
+        {contacts.map(contact => (<div key={contact.id} className="contact table-row">
+          <p className="first-name">{contact.name.split(" ")[0]}</p>
+          <p className="last-name">{contact.name.split(" ")[1]}</p>
+          <p className="phone">{contact.phone}</p>
+
+          <p className="actions">
+            <IconButton color="info">
+              <Edit/>
+            </IconButton>
+
+            <IconButton color="error">
+              <Delete/>
+            </IconButton>
+          </p>
+        </div>))}
+      </div>
     </>
   );
 };
